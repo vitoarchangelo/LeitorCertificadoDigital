21cbf2810e8f1a8af21a807a464a986d8508abe2
Vito Archangelo <vito.archangelo@gmail.com>
2018-06-26 15:35:08 -0300

    Altera inclusões do OpenSSL

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <iostream>
+
+
 #include <../OpenSSL/include/openssl/x509v3.h>
-
-
+#include <../OpenSSL/include/openssl/evp.h>
 
 
 
 
 int main() {
+
+	//OpenSSL_add_all_algorithms();
+
 	while (true)
 	{
 		printf("a \n");
