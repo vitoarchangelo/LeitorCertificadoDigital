61535c495449d21f36d80a3a038151eb98004654
Vito Archangelo <vito.archangelo@gmail.com>
2018-06-26 22:40:02 -0300

    Implementa leitura de um certificado

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,40 @@
 #include <iostream>
 
 
-#include <openssl/x509.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+#include <openssl/x509v3.h>
 
 
+#define MAX_CERT_LEN 20000
 
 int main() {
 
+	
 	OpenSSL_add_all_algorithms();
+	char certDer[MAX_CERT_LEN];
+	
+	FILE *fp = 0;
+	long len;
 
-	while (true)
-	{
-		printf("sera \n");
+	if (fp = fopen("../certificadoExemplo/cert.der", "rb")) {
+		len = fread(certDer, 1, MAX_CERT_LEN - 1, fp);
+		certDer[len] = '\0';
 	}
+
+	const unsigned char *buf = (const unsigned char *)certDer;
+	
+	X509* certificado = d2i_X509(NULL, &buf,len);
+	X509_NAME* subject = X509_get_subject_name(certificado);	
+	if (certificado != NULL)
+	{
+		const char *line = X509_NAME_oneline(subject, 0, 0);
+		printf("Subject: %s\n", line);
+	}
+	
+
+	fclose(fp);
 	return 0;
 }
+
